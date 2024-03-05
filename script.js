b5324c4ac219adcb559eac7187f76cb74fcd97f6
qwert <qwert@gmail.com>
2024-03-05 21:20:28 +0500

    firstMaster

---
+++ script.js
@@ -0,0 +1,24 @@
+var names = prompt('Введите свое имя')
+console.log('Ваше Имя: ' + names)
+var age = prompt('Введите свой возраст')
+console.log('Ваш возраст: ' + age)
+var lorem1 = +prompt('Решити пример 6 + 3 = ?')
+console.log('Пример 1: 6 + 3 = ' + (6 + 3) + ' Ваш ответ ' + lorem1);
+var lorem2 = +prompt('Решите пример 20 - 5 = ?')
+console.log('Пример 1: 20 - 5 = ' + (20 - 5) + ' Ваш ответ ' + lorem2);
+var lorem3 = +prompt('Решите пример 15 * 2 = ?')
+console.log('Пример 1: 15 * 2 = ' + (15 * 2) + ' Ваш ответ ' + lorem3);
+var lorem4 = +prompt('Решите пример 8 / 4 = ?')
+console.log('Пример 1: 8 / 4 = ' + (8 / 4) + ' Ваш ответ ' + lorem4);
+var lorem5 = +prompt('Решите пример 10 % 3 = ?')
+console.log('Пример 1: 10 % 3 = ' + (10 % 3) + ' Ваш ответ ' + lorem5);
+var lorem6 = alert('Откройте консоль')
+
+
+var firstNumber = +prompt('Введите первое число: ')
+var secondNumber = +prompt('Введите первое число: ')
+var thirdNumber = +prompt('Введите первое число: ')
+
+var res = ((firstNumber + secondNumber + thirdNumber) / 3)
+console.log(res)
+
